26eead3ce91c75dc10b5821fb2fa4a6b4a39ab3b
Whitney O'Meara <womeara@radiantblue.com>
2014-05-09 11:17:52 -0400

    Updated the unit test for local_min_max to include omp tests

--- test/filters/test_local_min_max.cpp
+++ test/filters/test_local_min_max.cpp
@@ -41,6 +41,7 @@
 
 #include <gtest/gtest.h>
 #include <pcl/filters/local_min_max.h>
+#include <pcl/filters/local_min_max_omp.h>
 #include <pcl/point_types.h>
 #include <stdio.h>
 
@@ -566,6 +567,602 @@ TEST (Grid, Maximum)
 
   EXPECT_EQ (cloud_out[0].z, 0.25f);
   EXPECT_EQ (cloud_out[1].z, 0.0f);
+  EXPECT_EQ (cloud_out.size (), 2);
+  EXPECT_EQ (lm.getResolution (), 2.0f);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Box_OMP, Minimum_Negative)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setResolution (1.0f);
+  lm.setStatType (lm.ST_MIN);
+  lm.setNegative (true);
+  lm.setLocalityType (lm.LT_BOX);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (0.25f, cloud_out[0].z);
+  EXPECT_EQ (1, cloud_out.size ());
+
+  lm.setResolution (10.0f);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (0.25f, cloud_out[0].z);
+  EXPECT_EQ (1, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Box_OMP, Minimum)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setResolution (1.0f);
+  lm.setStatType (lm.ST_MIN);
+  lm.setLocalityType (lm.LT_BOX);
+  lm.filter (cloud_out);
+
+  std::vector<double> results;
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  results.push_back (cloud_out[2].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (0.5f, results[0]);
+  EXPECT_EQ (1.0f, results[1]);
+  EXPECT_EQ (2.0f, results[2]);
+  EXPECT_EQ (3, cloud_out.size ());
+
+  lm.setResolution (10.0f);
+  lm.filter (cloud_out);
+
+  results.clear ();
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  results.push_back (cloud_out[2].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (0.5f, results[0]);
+  EXPECT_EQ (1.0f, results[1]);
+  EXPECT_EQ (2.0f, results[2]);
+  EXPECT_EQ (3, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Box_OMP, Maximum_Negative)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setResolution (1.0f);
+  lm.setStatType (lm.ST_MAX);
+  lm.setNegative (true);
+  lm.setLocalityType (lm.LT_BOX);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (1.0f, cloud_out[0].z);
+  EXPECT_EQ (1, cloud_out.size ());
+
+  lm.setResolution (10.0f);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (2.0f, cloud_out[0].z);
+  EXPECT_EQ (1, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Box_OMP, Maximum)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setResolution (1.0f);
+  lm.setStatType (lm.ST_MAX);
+  lm.setLocalityType (lm.LT_BOX);
+  lm.filter (cloud_out);
+
+  std::vector<double> results;
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  results.push_back (cloud_out[2].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (0.25f, results[0]);
+  EXPECT_EQ (0.5f, results[1]);
+  EXPECT_EQ (2.0f, results[2]);
+  EXPECT_EQ (3, cloud_out.size ());
+
+  lm.setResolution (10.0f);
+  lm.filter (cloud_out);
+
+  results.clear ();
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  results.push_back (cloud_out[2].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (0.25f, results[0]);
+  EXPECT_EQ (0.5f, results[1]);
+  EXPECT_EQ (1.0f, results[2]);
+  EXPECT_EQ (3, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Radius_OMP, Minimum_Negative)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setRadius (1.0f);
+  lm.setStatType (lm.ST_MIN);
+  lm.setNegative (true);
+  lm.setLocalityType (lm.LT_RADIUS);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (0.25f, cloud_out[0].z);
+  EXPECT_EQ (1, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Radius_OMP, Minimum)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setRadius (1.0f);
+  lm.setStatType (lm.ST_MIN);
+  lm.setLocalityType (lm.LT_RADIUS);
+  lm.filter (cloud_out);
+
+  std::vector<double> results;
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  results.push_back (cloud_out[2].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (0.5f, results[0]);
+  EXPECT_EQ (1.0f, results[1]);
+  EXPECT_EQ (2.0f, results[2]);
+  EXPECT_EQ (3, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Radius_OMP, Maximum_Negative)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setRadius (1.0f);
+  lm.setStatType (lm.ST_MAX);
+  lm.setNegative (true);
+  lm.setLocalityType (lm.LT_RADIUS);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (1.0f, cloud_out[0].z);
+  EXPECT_EQ (1, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Radius_OMP, Maximum)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setRadius (1.0f);
+  lm.setStatType (lm.ST_MAX);
+  lm.setLocalityType (lm.LT_RADIUS);
+  lm.filter (cloud_out);
+
+  std::vector<double> results;
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  results.push_back (cloud_out[2].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (0.25f, results[0]);
+  EXPECT_EQ (0.50f, results[1]);
+  EXPECT_EQ (2.0f, results[2]);
+  EXPECT_EQ (3, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (KNN_OMP, Minimum_Negative)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setNumNeighbors (2);
+  lm.setStatType (lm.ST_MIN);
+  lm.setNegative (true);
+  lm.setLocalityType (lm.LT_KNN);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (0.25f, cloud_out[0].z);
+  EXPECT_EQ (1, cloud_out.size ());
+
+  lm.setNumNeighbors (1);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (0.25f, cloud_out[0].z);
+  EXPECT_EQ (1, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (KNN_OMP, Minimum)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setNumNeighbors (2);
+  lm.setStatType (lm.ST_MIN);
+  lm.setLocalityType (lm.LT_KNN);
+  lm.filter (cloud_out);
+
+  std::vector<double> results;
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  results.push_back (cloud_out[2].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (0.5f, results[0]);
+  EXPECT_EQ (1.0f, results[1]);
+  EXPECT_EQ (2.0f, results[2]);
+  EXPECT_EQ (3, cloud_out.size ());
+
+  lm.setNumNeighbors (1);
+  lm.filter (cloud_out);
+
+  results.clear ();
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  results.push_back (cloud_out[2].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (0.5f, results[0]);
+  EXPECT_EQ (1.0f, results[1]);
+  EXPECT_EQ (2.0f, results[2]);
+  EXPECT_EQ (3, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (KNN_OMP, Maximum_Negative)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setNumNeighbors (2);
+  lm.setStatType (lm.ST_MAX);
+  lm.setNegative (true);
+  lm.setLocalityType (lm.LT_KNN);
+  lm.filter (cloud_out);
+
+  std::vector<double> results;
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (1.0f, results[0]);
+  EXPECT_EQ (2.0f, results[1]);
+  EXPECT_EQ (2, cloud_out.size ());
+
+  lm.setNumNeighbors (1);
+  lm.filter (cloud_out);
+
+  results.clear ();
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  results.push_back (cloud_out[2].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (0.5f, results[0]);
+  EXPECT_EQ (1.0f, results[1]);
+  EXPECT_EQ (2.0f, results[2]);
+  EXPECT_EQ (3, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (KNN_OMP, Maximum)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (4);
+
+  cloud_in[0].x = 0;    cloud_in[0].y = 0;    cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.25; cloud_in[1].y = 0.25; cloud_in[1].z = 0.5;
+  cloud_in[2].x = 0.5;  cloud_in[2].y = 0.5;  cloud_in[2].z = 1;
+  cloud_in[3].x = 5;    cloud_in[3].y = 5;    cloud_in[3].z = 2;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setNumNeighbors (2);
+  lm.setStatType (lm.ST_MAX);
+  lm.setLocalityType (lm.LT_KNN);
+  lm.filter (cloud_out);
+
+  std::vector<double> results;
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (0.25f, results[0]);
+  EXPECT_EQ (0.5f, results[1]);
+  EXPECT_EQ (2, cloud_out.size ());
+
+  lm.setNumNeighbors (1);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (0.25f, cloud_out[0].z);
+  EXPECT_EQ (1, cloud_out.size ());
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Grid_OMP, Minimum_Negative)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (3);
+
+  cloud_in[0].x = 0;   cloud_in[0].y = 0;   cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.5; cloud_in[1].y = 0.5; cloud_in[1].z = 1;
+  cloud_in[2].x = 1.5; cloud_in[2].y = 1.5; cloud_in[2].z = 0.0;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setResolution (1.0f);
+  lm.setLocalityType (lm.LT_GRID);
+  lm.setStatType (lm.ST_MIN);
+  lm.setNegative (true);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (cloud_out[0].z, 0.25f);
+  EXPECT_EQ (cloud_out.size (), 1);
+  EXPECT_EQ (lm.getResolution (), 1.0f);
+
+  lm.setResolution (2.0f);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (cloud_out[0].z, 0.0f);
+  EXPECT_EQ (cloud_out.size (), 1);
+  EXPECT_EQ (lm.getResolution (), 2.0f);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Grid_OMP, Minimum)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (3);
+
+  cloud_in[0].x = 0;   cloud_in[0].y = 0;   cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.5; cloud_in[1].y = 0.5; cloud_in[1].z = 1;
+  cloud_in[2].x = 1.5; cloud_in[2].y = 1.5; cloud_in[2].z = 0.0;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setResolution (1.0f);
+  lm.setLocalityType (lm.LT_GRID);
+  lm.setStatType (lm.ST_MIN);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (cloud_out[0].z, 1.0f);
+  EXPECT_EQ (cloud_out.size (), 1);
+  EXPECT_EQ (lm.getResolution (), 1.0f);
+
+  lm.setResolution (2.0f);
+  lm.filter (cloud_out);
+
+  std::vector<double> results;
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (results[0], 0.25f);
+  EXPECT_EQ (results[1], 1.0f);
+  EXPECT_EQ (cloud_out.size (), 2);
+  EXPECT_EQ (lm.getResolution (), 2.0f);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Grid_OMP, Maximum_Negative)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (3);
+
+  cloud_in[0].x = 0;   cloud_in[0].y = 0;   cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.5; cloud_in[1].y = 0.5; cloud_in[1].z = 1;
+  cloud_in[2].x = 1.5; cloud_in[2].y = 1.5; cloud_in[2].z = 0.0;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setResolution (1.0f);
+  lm.setLocalityType (lm.LT_GRID);
+  lm.setStatType (lm.ST_MAX);
+  lm.setNegative (true);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (cloud_out[0].z, 1.0f);
+  EXPECT_EQ (cloud_out.size (), 1);
+  EXPECT_EQ (lm.getResolution (), 1.0f);
+
+  lm.setResolution (2.0f);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (cloud_out[0].z, 1.0f);
+  EXPECT_EQ (cloud_out.size (), 1);
+  EXPECT_EQ (lm.getResolution (), 2.0f);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST (Grid_OMP, Maximum)
+{
+  PointCloud<PointXYZ> cloud_in, cloud_out;
+
+  cloud_in.height = 1;
+  cloud_in.width = 3;
+  cloud_in.is_dense = true;
+  cloud_in.resize (3);
+
+  cloud_in[0].x = 0;   cloud_in[0].y = 0;   cloud_in[0].z = 0.25;
+  cloud_in[1].x = 0.5; cloud_in[1].y = 0.5; cloud_in[1].z = 1;
+  cloud_in[2].x = 1.5; cloud_in[2].y = 1.5; cloud_in[2].z = 0.0;
+
+  LocalMinMaxOMP<PointXYZ> lm;
+  lm.setInputCloud (cloud_in.makeShared ());
+  lm.setResolution (1.0f);
+  lm.setLocalityType (lm.LT_GRID);
+  lm.setStatType (lm.ST_MAX);
+  lm.filter (cloud_out);
+
+  EXPECT_EQ (cloud_out[0].z, 0.25f);
+  EXPECT_EQ (cloud_out.size (), 1);
+  EXPECT_EQ (lm.getResolution (), 1.0f);
+
+  lm.setResolution (2.0f);
+  lm.filter (cloud_out);
+
+  std::vector<double> results;
+  results.push_back (cloud_out[0].z);
+  results.push_back (cloud_out[1].z);
+  std::sort (results.begin (), results.end ());
+
+  EXPECT_EQ (results[0], 0.0f);
+  EXPECT_EQ (results[1], 0.25f);
   EXPECT_EQ (cloud_out.size (), 2);
   EXPECT_EQ (lm.getResolution (), 2.0f);
 }
